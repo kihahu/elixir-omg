d41144db567c53c12401e2f49a1633118ec97b2f
pthomalla <pawel.thomalla@imapp.pl>
2018-08-01 11:39:54 +0200

    add simple form_block

--- apps/omisego_api/test/state/prop_my_test.exs
+++ apps/omisego_api/test/state/prop_my_test.exs
@@ -38,7 +38,7 @@ defmodule OmiseGO.API.State.PropMYTest do
 
   # TODO: make aggregation and statistics informative
   # [:verbose, :noshrink, max_size: 10, constraint_tries: 1, numtests: 3, start_size: 3]
-  property "OmiseGO.API.State.Core prope check", max_size: 100, numtests: 100, start_size: 1 do
+  property "OmiseGO.API.State.Core prope check", max_size: 100, numtests: 10, start_size: 1 do
     forall cmds <- commands(__MODULE__) do
       trap_exit do
         print("============> start run", {20, 60, 80})
@@ -66,18 +66,15 @@ defmodule OmiseGO.API.State.PropMYTest do
     end
   end
 
-  # def initial_state do
-  #  print("initial_state", {40, 80, 52})
-  #  {:ok, state} = Core.extract_initial_state([], 0, 0, 1000)
-  #  CoreGS2.set_state(state)
-  #
-  #  %{
-  #    model: %{history: []},
-  #    eth: %{blknum: 1_000}
-  #  }
-  # end
   def initial_state do
-    [1]
+    print("initial_state", {40, 80, 52})
+    {:ok, state} = Core.extract_initial_state([], 0, 0, 1000)
+    CoreGS2.set_state(state)
+
+    %{
+      model: %{history: []},
+      eth: %{blknum: 1_000}
+    }
   end
 
   def address do
@@ -96,87 +93,80 @@ defmodule OmiseGO.API.State.PropMYTest do
     <<0::160>>
   end
 
-  # defcommand :deposit do
-  #  def impl(arg) do
-  #    CoreGS2.deposit(arg)
-  #  end
-  #
-  #  def args(%{eth: %{blknum: blknum}} = str) do
-  #    state_ful = CoreGS2.get_state()
-  #    IO.puts(inspect(state_ful))
-  #    IO.puts(inspect(str))
-  #
-  #    let [number_of_deposit <- oneof([1, 2, 3, 4]), amount <- pos_integer()] do
-  #      # number_of_deposit = :rand.uniform(4)
-  #
-  #      deposits =
-  #        for number <- 1..number_of_deposit,
-  #            do: %{blknum: blknum + number, currency: currency(), owner: address(), amount: amount}
-  #
-  #      [deposits]
-  #    end
-  #  end
-  #
-  #  def pre(%{eth: %{blknum: blknum}}, [list_deposits]) do
-  #    IO.puts(inspect(blknum))
-  #    list_block = list_deposits |> Enum.map(fn %{blknum: blknum} -> blknum end)
-  #    expected = for i <- (blknum + 1)..(blknum + length(list_deposits)), do: i
-  #    IO.puts(inspect(expected) <> " == " <> inspect(list_block))
-  #    IO.puts("pre blknum: #{inspect(blknum)}")
-  #    ret = rem(blknum, 1000) + length(list_deposits) < 1000 and expected == list_block
-  #    IO.puts(inspect(ret))
-  #    ret
-  #  end
-  #
-  #  def post(entries, [arg], {:ok, {_, dp_update}}) do
-  #    new_utxo =
-  #      dp_update
-  #      |> Enum.filter(fn
-  #        {:put, :utxo, _} -> true
-  #        _ -> false
-  #      end)
-  #      |> length
-  #
-  #    ret = length(arg) == new_utxo
-  #    IO.puts("POST #{inspect(length(arg))} == #{inspect(new_utxo)}\t#{inspect(ret)}")
-  #    IO.puts("OST2 #{inspect(CoreGS2.get_state())}")
-  #    ret
-  #  end
-  #
-  #  def next(state, _, {:var, _}), do: state
-  #
-  #  def next(%{eth: %{blknum: blknum} = eth} = state, [args], {:ok, _}) do
-  #    # IO.puts(inspect(args))
-  #    state = %{state | eth: %{eth | blknum: blknum + length(args)}}
-  #    IO.puts("next state: #{inspect(state)}")
-  #    state
-  #  end
-  #
-  #  def next(state, args, wtf) do
-  #    IO.puts("wtf ????: " <> inspect(wtf))
-  #    state
-  #  end
-  # end
-
   defcommand :deposit do
-    def impl(arg) do
-      print(arg)
-      arg + 1
+    def impl(deposits), do: CoreGS2.deposit(deposits)
+
+    def args(%{eth: %{blknum: blknum}} = str) do
+      let [number_of_deposit <- integer(1, 20), amount <- pos_integer()] do
+        deposits =
+          for number <- 1..number_of_deposit,
+              do: %{blknum: blknum + number, currency: currency(), owner: address(), amount: amount}
+
+        [deposits]
+      end
     end
 
-    def args([state]) do
-      "args:[#{1 + state}]" |> background({10, 10, 10}) |> IO.puts()
-      [1 + state]
+    def pre(%{eth: %{blknum: blknum}}, [deposits]) do
+      list_block = deposits |> Enum.map(fn %{blknum: blknum} -> blknum end)
+      expected = for i <- (blknum + 1)..(blknum + length(deposits)), do: i
+      rem(blknum, 1000) + length(deposits) < 1000 and expected == list_block
     end
 
-    def pre([state], [arg]) do
-      IO.puts("pre: #{inspect(state)}, arg: #{inspect(arg)}")
+    def post(_state, [arg], {:ok, {_, dp_update}}) do
+      new_utxo =
+        dp_update
+        |> Enum.filter(fn
+          {:put, :utxo, _} -> true
+          _ -> false
+        end)
+        |> length
+
+      length(arg) == new_utxo
+    end
+
+    def next(%{eth: %{blknum: blknum} = eth, model: %{history: history} = modle} = state, [args], ret) do
+      %{
+        state
+        | eth: %{eth | blknum: blknum + length(args)},
+          model: %{modle | history: [{:deposit, args, ret} | history]}
+      }
+    end
+  end
+
+  defcommand :form_block do
+    def impl() do
+      Logger.info("form _block")
+      CoreGS2.form_block(1_000)
+    end
+
+    def post(state, [], ret) do
       true
     end
 
-    def next([state], [arg], _res) do
-      "deposit state change:#{state} to: #{arg}" |> background({10, 10, 10}) |> IO.puts()
-      [arg]
+    def next(state, args, ret) do
+      state
     end
   end
+
+  # defcommand :deposit do
+  #  def impl(arg) do
+  #    print(arg)
+  #    arg + 1
+  #  end
+  #
+  #  def args([state]) do
+  #    "args:[#{1 + state}]" |> background({10, 10, 10}) |> IO.puts()
+  #    [1 + state]
+  #  end
+  #
+  #  def pre([state], [arg]) do
+  #    IO.puts("pre: #{inspect(state)}, arg: #{inspect(arg)}")
+  #    true
+  #  end
+  #
+  #  def next([state], [arg], res) do
+  #    "deposit state change:#{state} to: #{arg}  res: #{inspect(res)}" |> background({10, 10, 10}) |> IO.puts()
+  #    [arg]
+  #  end
+  # end
 end
