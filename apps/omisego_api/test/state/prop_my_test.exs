3fbf5f612fcefe3c01e827c12d2794cffc1690b1
pthomalla <pawel.thomalla@imapp.pl>
2018-08-10 12:38:21 +0200

    stable work with standard command

--- apps/omisego_api/test/state/prop_my_test.exs
+++ apps/omisego_api/test/state/prop_my_test.exs
@@ -2,12 +2,13 @@ defmodule OmiseGO.API.State.PropMYTest do
   @moduledoc """
   Defines the state machine for chain state.
   """
-
+  import OmiseGO.API.State.PropTest.Deposits
   use PropCheck
   use PropCheck.StateM.DSL
   import PropCheck.BasicTypes
   use ExUnit.Case
   alias OmiseGO.API.State.{Core, Transaction}
+  alias OmiseGO.API.Block
 
   use OmiseGO.API.LoggerExt
   alias OmiseGO.API.LoggerExt
@@ -18,18 +19,15 @@ defmodule OmiseGO.API.State.PropMYTest do
 
   @moduletag :wip11
 
-  defp insp(value), do: value |> inspect()
-  defp print(value, rgb \\ {40, 44, 52}), do: value |> insp |> background(rgb) |> IO.puts()
-
   defp background(str, {r, g, b}),
     do: String.replace(IO.ANSI.reset() <> str, IO.ANSI.reset(), "\e[48;2;#{r};#{g};#{b}m") <> IO.ANSI.reset()
 
+  defp print(value, rgb \\ {40, 44, 52}), do: value |> inspect |> background(rgb) |> IO.puts()
   # TODO: make aggregation and statistics informative
   # [:verbose, :noshrink, max_size: 10, constraint_tries: 1, numtests: 3, start_size: 3]
-  property "OmiseGO.API.State.Core prope check", [:quiet, max_size: 6, numtests: 60, start_size: 3] do
+  property "OmiseGO.API.State.Core prope check", [:quiet, max_size: 300, numtests: 10, start_size: 3] do
     forall cmds <- commands(__MODULE__) do
       trap_exit do
-        print("============> start run", {20, 60, 80})
         # print(cmds)
         %{history: history, result: result, state: state, env: _env} = response = run_commands(cmds)
         # print(response, {80, 80, 40})
@@ -37,32 +35,29 @@ defmodule OmiseGO.API.State.PropMYTest do
         ret =
           (result == :ok)
           |> when_fail(
-            """
-            Result: #{inspect(result)}
-            Commands: #{inspect(cmds)}
-            History: #{inspect(history)}
-            State: #{inspect(state)}
-            """
-            |> IO.puts()
+            (fn ->
+               print("Commands: #{inspect(cmds)}", {40, 44, 92})
+               Logger.warn("State: #{inspect(state)}")
+               # IO.puts("History: #{inspect(history)}")
+               Logger.error("Result: #{inspect(result)}")
+             end).()
           )
           |> aggregate(command_names(cmds))
           |> collect(length(cmds))
 
-        print("============> stop", {20, 60, 80})
+        print("=> stop", {20, 60, 80})
         ret
       end
     end
   end
 
   def initial_state do
-    print("initial_state", {40, 80, 52})
     {:ok, state} = Core.extract_initial_state([], 0, 0, 1000)
     CoreGS2.set_state(state)
 
     %{
       model: %{history: []},
       eth: %{blknum: 1_000}
-      # ,utxo: []
     }
   end
 
@@ -78,95 +73,92 @@ defmodule OmiseGO.API.State.PropMYTest do
     frequency([{10, <<0::160>>}, {1, <<1::160>>}])
   end
 
-  defcommand :deposits do
-    def impl(deposits), do: CoreGS2.deposit(deposits)
+  defcommand :form_block do
+    def impl(), do: CoreGS2.form_block(1_000)
 
-    def args(%{eth: %{blknum: blknum}} = str) do
-      let [number_of_deposit <- integer(1, 2)] do
-        [
-          for number <- 1..number_of_deposit do
-            let [currency <- get_currency(), %{addr: owner} <- entitie(), amount <- integer(30, 300_000)] do
-              %{blknum: blknum + number, currency: currency, owner: owner, amount: amount}
-            end
-          end
-        ]
-      end
-    end
-
-    def pre(%{eth: %{blknum: blknum}}, [deposits]) do
-      list_block = deposits |> Enum.map(fn %{blknum: blknum} -> blknum end)
-      expected = for i <- (blknum + 1)..(blknum + length(deposits)), do: i
-      rem(blknum, 1000) + length(deposits) < 1000 and expected == list_block
-    end
-
-    def post(_state, [arg], {:ok, {_, dp_update}}) do
-      new_utxo =
-        dp_update
-        |> Enum.filter(fn
-          {:put, :utxo, _} -> true
-          _ -> false
+    def post(%{model: %{history: history}}, [], {:ok, {%Block{transactions: transaction}, _, _}}) do
+      expected_transactions =
+        history
+        |> Enum.take_while(&(!match?({:form_block, _, _}, &1)))
+        |> Enum.filter(&match?({:transaction, _, _}, &1))
+        |> Enum.map(fn {:transaction,
+                        [%Transaction.Recovered{signed_tx: %Transaction.Signed{signed_tx_bytes: tx_bytes}}, _], _} ->
+          tx_bytes
         end)
-        |> length
+        |> Enum.reverse()
 
-      length(arg) == new_utxo
+      expected_transactions == transaction
     end
 
-    def next(%{eth: %{blknum: blknum} = eth, model: %{history: history} = model} = state, [args], ret) do
+    def next(%{model: %{history: history} = model, eth: %{blknum: number} = eth} = state, [], ret) do
       %{
         state
-        | eth: %{eth | blknum: blknum + length(args)},
-          model: %{model | history: [{:deposits, args, ret} | history]}
+        | eth: %{eth | blknum: (div(number, 1000) + 1) * 1000},
+          model: %{model | history: [{:form_block, [], ret} | history]}
       }
     end
   end
 
-  defcommand :form_block do
-    def impl() do
-      CoreGS2.form_block(1_000)
-    rescue
-      msg ->
-        state = CoreGS2.get_state()
-        IO.puts(inspect(state))
-        Logger.error("wtf #{inspect(msg)} state: #{inspect(state)} ")
-        :error
-    end
-
-    # {:ok, {block, tx_events, db_update}}) do
-    def post(state, [], ret) do
-      # TODO add check transsaction in block with history
-      :error != ret
-    end
-
-    def next(%{model: %{history: history} = model} = state, args, ret) do
-      %{state | model: %{model | history: [{:form_block, args, ret} | history]}}
-    end
-  end
-
   def spendable(history) do
-    spendable(Enum.reverse(history), %{})
+    history = Enum.reverse(history)
+    spendable(history, %{})
   end
 
   def spendable([{:deposits, utxos, _} | history], unspent) do
     entities = OmiseGO.API.TestHelper.entities_stable()
 
-    utxos
-    |> Enum.map(&Map.merge(&1, %{oindex: 0, txindex: 0}))
-    |> Enum.reduce(unspent, fn %{
-                                 owner: owner,
-                                 amount: amount,
-                                 blknum: blknum,
-                                 currency: currency,
-                                 oindex: oindex,
-                                 txindex: txindex
-                               },
-                               acc ->
-      {_, owner} = Enum.find(entities, fn element -> match?({_, %{addr: ^owner}}, element) end)
-      Map.put_new(acc, {blknum, txindex, oindex, owner}, %{amount: amount, currency: currency})
-    end)
+    unspent =
+      utxos
+      |> Enum.map(&Map.merge(&1, %{oindex: 0, txindex: 0}))
+      |> Enum.reduce(unspent, fn %{
+                                   owner: owner,
+                                   amount: amount,
+                                   blknum: blknum,
+                                   currency: currency,
+                                   oindex: oindex,
+                                   txindex: txindex
+                                 },
+                                 acc ->
+        {_, owner} = Enum.find(entities, fn element -> match?({_, %{addr: ^owner}}, element) end)
+        Map.put_new(acc, {blknum, txindex, oindex}, %{amount: amount, currency: currency, owner: owner})
+      end)
+
+    spendable(history, unspent)
+  end
+
+  def spendable([{:form_block, [], _} | history], unspent), do: spendable(history, unspent)
+
+  def spendable(
+        [
+          {:transaction,
+           [
+             %Transaction.Recovered{
+               signed_tx: %Transaction.Signed{
+                 raw_tx: %Transaction{
+                   blknum1: blknum1,
+                   txindex1: txindex1,
+                   oindex1: oindex1,
+                   blknum2: blknum2,
+                   txindex2: txindex2,
+                   oindex2: oindex2
+                 }
+               }
+             },
+             _
+           ], _}
+          | history
+        ],
+        unspent
+      ) do
+    {_, unspent} = Map.pop(unspent, {blknum1, txindex1, oindex1})
+    {_, unspent} = Map.pop(unspent, {blknum2, txindex2, oindex2})
+    spendable(history, unspent)
   end
 
   def spendable([why | history], unspent) do
-    Logger.warn("#{inspect(why)}")
+    Logger.warn("spendable")
+    print(why, {80, 40, 50})
+    throw("ble")
     spendable(history, unspent)
   end
 
@@ -181,8 +173,6 @@ defmodule OmiseGO.API.State.PropMYTest do
 
   defcommand :transaction do
     def impl(transaction, fee_map) do
-      Logger.info(inspect(transaction))
-      Logger.warn(inspect(fee_map))
       CoreGS2.exec(transaction, fee_map)
     end
 
@@ -193,7 +183,7 @@ defmodule OmiseGO.API.State.PropMYTest do
         spentable = [nil | :maps.filter(fn _, %{currency: val} -> val == currency end, spentable) |> Map.to_list()]
 
         let [utxo1 <- oneof(spentable), utxo2 <- oneof(spentable)] do
-          utxos = [utxo1, utxo2] |> Enum.filter(&(&1 != nil))
+          utxos = [utxo1, utxo2] |> Enum.filter(&(&1 != nil)) |> Enum.uniq()
           total_amount = Enum.reduce(utxos, 0, fn {_, %{amount: amount}}, acc -> amount + acc end)
 
           let [amount1 <- range(0, Enum.max([total_amount - 10, 0])), fee <- range(0, 10)] do
@@ -201,7 +191,7 @@ defmodule OmiseGO.API.State.PropMYTest do
 
             [
               OmiseGO.API.TestHelper.create_recovered(
-                utxos |> Enum.map(&elem(&1, 0)),
+                utxos |> Enum.map(fn {position, %{owner: owner}} -> Tuple.append(position, owner) end),
                 currency,
                 Enum.zip(Enum.take([new_owner1, new_owner2], new_owners), [amount1, amount2])
               ),
@@ -212,16 +202,33 @@ defmodule OmiseGO.API.State.PropMYTest do
       end
     end
 
-    def pre(_model, [
+    def pre(%{model: %{history: history}}, [
           %Transaction.Recovered{
-            signed_tx: %Transaction.Signed{raw_tx: %Transaction{amount1: amount1, amount2: amount2, cur12: currency}},
+            signed_tx: %Transaction.Signed{
+              raw_tx: %Transaction{
+                amount1: amount1,
+                amount2: amount2,
+                cur12: currency,
+                blknum1: blknum1,
+                txindex1: txindex1,
+                oindex1: oindex1,
+                blknum2: blknum2,
+                txindex2: txindex2,
+                oindex2: oindex2
+              }
+            },
             spender1: spender1,
             spender2: spender2
           },
           fees_map
         ]) do
+      spendable = spendable(history)
       spender2_valid = if amount2 > 0, do: spender2 != nil, else: true
-      amount1 >= 0 and amount2 >= 0 and spender1 != nil and spender2_valid
+
+      amount1 >= 0 and amount2 >= 0 and spender1 != nil and spender2_valid and
+        Map.has_key?(spendable, {blknum1, txindex1, oindex1}) and Map.has_key?(spendable, {blknum2, txindex2, oindex2}) and
+        Map.get(spendable, {blknum1, txindex1, oindex1}).currency == currency and
+        Map.get(spendable, {blknum2, txindex2, oindex2}).currency == currency
     end
 
     def pre(_model, _any), do: false
@@ -234,7 +241,6 @@ defmodule OmiseGO.API.State.PropMYTest do
     end
 
     def post(_state, args, response) do
-      Logger.info(inspect(response))
       true
     end
 
