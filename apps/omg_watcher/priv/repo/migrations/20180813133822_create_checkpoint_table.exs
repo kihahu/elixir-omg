d61addd84202e94b15eb37ec16b05236a8e22dd2
Pawel Nowosielski <Pawel.Nowosielski@imapp.pl>
2018-08-27 16:42:12 +0200

    add new migrations for WatcherDb database's tables

---
+++ apps/omg_watcher/priv/repo/migrations/20180813133822_create_checkpoint_table.exs
@@ -0,0 +1,13 @@
+defmodule OmiseGOWatcher.Repo.Migrations.CreateCheckpointTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:checkpoints, primary_key: false) do
+      add :blknum, :bigint, primary_key: true
+      add :hash, :binary, null: false
+      add :eth_height, :bigint, null: false
+    end
+
+    create unique_index(:checkpoints, [:hash])
+  end
+end
